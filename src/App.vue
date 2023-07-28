f1a2d265f3d3a02001c92876da16f5a981f56184
seoyeon0201 <psy0201123@gmail.com>
2023-07-28 16:07:43 +0900

    Vue.js first

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>{{ name }}</p>
+  <button class="btn btn-primary" v-on:click="updateName">Click</button>
+</template>
+ 
+<script>
+import {ref} from 'vue';
+
+export default {
+  setup() {
+    const name = ref('ParkSeoYeon');
+
+    const updateName = () => {
+      name.value = 'SeoYeon';
+      console.log(name);
+    }
+
+    return {
+      name, updateName
+    }
+  }
+}
+</script>
+
+<!--style: CSS 코드 작성-->
+<style>
+  .name {
+    color: grey;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+//index.html의 app에 App(즉 ./App.vue) 들어감
+createApp(App).mount('#app')   
